8f66993c2a3da3e3578063a4427491ea2b903d65
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-07-17 17:36:32 -0600

    Letras

---
+++ src/main/java/com/bdelas/microservice3gateway/Microservice3GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bdelas.microservice3gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice3GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice3GatewayApplication.class, args);
+	}
+
+}
