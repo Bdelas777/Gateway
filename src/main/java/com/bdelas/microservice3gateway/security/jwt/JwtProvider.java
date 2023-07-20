b9efa14e213812ee0a3ee892968acc61c450eb80
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-07-19 19:10:14 -0600

    Token

--- src/main/java/com/bdelas/microservice3gateway/security/jwt/JwtProvider.java
+++ src/main/java/com/bdelas/microservice3gateway/security/jwt/JwtProvider.java
@@ -1,4 +1,18 @@
 package com.bdelas.microservice3gateway.security.jwt;
 
+import com.bdelas.microservice3gateway.security.UserPrincipal;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
 public interface JwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    // propiedad de los lenguajes al interior del token
+    Claims extractClaims(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
 }
